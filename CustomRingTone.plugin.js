81da2994c1a9c1bc8199e7a5caedcb0f31c568c9
humbulit <humbulitJG@gmail.com>
2022-12-03 16:56:19 -0500

    Update CustomRingTone.plugin.js

--- CustomRingTone.plugin.js
+++ CustomRingTone.plugin.js
@@ -4,11 +4,9 @@
  * @version 1.0.0
  * @author humbulit#0001
  * @authorId 504808783176794116
- * @updateUrl https://raw.githubusercontent.com/
- * @website https://github.com/
- */
-
-const ytdl = require('ytdl-core');
+ * @updateUrl https://raw.githubusercontent.com/humbulit/BDCustomRingTone/main/CustomRingTone.plugin.js
+ * @website https://github.com/humbulit/BDCustomRingTone/blob/main/CustomRingTone.plugin.js
+*/
 
 const config = {
         info: {
@@ -24,16 +22,13 @@ const config = {
         defaultConfig: []
 };
 
-module.exports = class name {
+module.exports = class CustomRingTone {
 
         constructor() {
                 this._config = config;
         }
 
-	load() { }
-
 	start() {
-                ytdl(song.url, { quality: 'highestaudio', filter: 'audio' })
 		Audio.prototype._play = Audio.prototype.play;
                 Audio.prototype.play = function() {
                         this.src = this.src.replace(/84a1b4e11d634dbfa1e5dd97a96de3ad/g, 'b9411af07f154a6fef543e7e442e4da9');
